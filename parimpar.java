274b83fbd7d141a47029d74e4dacdb4cd49835a1
Francisco-Alamino-Neto <franciscoalaminoneto26@gmail.com>
2025-03-17 11:07:31 -0300

    Add files via upload

---
+++ parimpar.java
@@ -0,0 +1,26 @@
+package exerciciosLoop;
+
+import java.util.Scanner;
+
+public class parimpar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int numero = 1;
+
+		while (numero != 0) {
+
+			System.out.print("Digite um número: ");
+			numero = sc.nextInt();
+
+			if (numero % 2 == 0) {
+				System.out.println(numero + " é um número par.");
+			} else {
+				System.out.println(numero + " é um número ímpar.");
+			}
+
+		}
+	}
+
+}
